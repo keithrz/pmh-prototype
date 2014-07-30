c9da6e3912fa5a37fdde102f0ecf15703b620522
Keith Zimmerman <keithrz@gmail.com>
2014-07-30 09:55:56 -0500

    Started logic for creating a user's trip, based on dummy trail data.

--- js/controller/StartCtrl.js
+++ js/controller/StartCtrl.js
@@ -5,7 +5,7 @@ angular.module('pmh').controller('StartCtrl', [
         'use strict';
 
         $scope.trip = {
-            route: "PCT",
+            trail: "PCT",
             startDate: new Date(),
             hoursPerDay: 8,
             pace: 2,
@@ -15,4 +15,40 @@ angular.module('pmh').controller('StartCtrl', [
             timeOffFrequency: 'section'
         };
 
+		$scope.startTrip = function() {
+//			var trailSections = trailService.getSections($scope.trip.trail);
+			var trailSections = $scope.getSections($scope.trip.trail);
+			$scope.createTripSections(trailSections, $scope.trip)
+		};
+
+		$scope.createTripSections = function(trailSections, trip) {
+			var tripDate = trip.startDate,
+			    tripSections = [];
+			
+			forEach(trailSections, function(trailSection) {
+				var tripSection = {};
+				tripSection.name = trailSection.name;
+				tripSection.startDate = tripDate;
+				//todo convert pace using pace uom
+				//todo don't multiply by hpd (and disable field) if pace uom is per day
+				tripSection.days = trailSection.distance / (pace * hoursPerDay);
+				//todo determine rounding precision for days
+				tripDate += tripSection.days;
+				tripSection.endDate = tripDate
+				tripSections.push(tripSection);
+			});
+		};
+
+		$scope.getSections = function(trail) {
+			if(trail === 'PCT') {
+				return [{
+					{
+						name: "Steven's Pass to Stehekin",
+						distance: 100
+					},{
+						name: "Stehekin to Border",
+						distance: 80
+				}]
+			}
+		};
     }]);
